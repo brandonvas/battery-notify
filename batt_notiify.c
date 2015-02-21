12a7c7e3ab2dcc450561f86a7a3c1358168bb221
Brandon Vasquez <brandonvas@gmail.com>
2015-02-21 17:09:47 -0500

    Changed from using acpi to looking at the sys/class/power_supply/BAT0/uevent file

--- batt_notiify.c
+++ batt_notiify.c
@@ -4,38 +4,106 @@
 #include <unistd.h>
 
 #define CRITICAL 10
-#define LOW      20
+#define LOW      15
 #define BATTERY_STRING_LENGTH 40
 #define UPDATE_RATE 10 // In seconds
 #define NOTIFICATION_TIMEOUT 5000 // In ms, with a minimum of 1 second
 
-int get_battery_string(char * battery_string) {
-    FILE *fp;
-    fp = popen("/usr/bin/acpi", "r");
-    if (!fp) {
-        fprintf(stderr, "Unable to execute acpi command from battery notifier");
-        return 1;
-    }
+struct Battery {
+    int status;             // 0 = discharging, 1 = charging
+    float energy_now;         // current energy level
+    float energy_full_design; // manufactured energy level
+    float energy_full;        // energy level during last charge
+};
 
-    fgets(battery_string, BATTERY_STRING_LENGTH, fp);
-    pclose(fp);
+int get_ith_split(char * string, char * delimiter, int ith, char * str_result) {
+    int i;
+    char * pch;
+
+    pch = strtok(string, delimiter);
+    if(pch == NULL) return -1;
+
+    for (i = ith; i > 0; i--) {
+        pch = strtok(NULL, delimiter);
+        if(pch == NULL) return -1;
+    }
+    strncpy(str_result, pch, 100);
     return 0;
 }
 
-// Parses the battery string to get the battery percentage
-int get_battery_percent(char * battery_string) {
+int get_battery_info(struct Battery * batt) {
 
-    char * pch;
-    pch = strtok(battery_string, " ");
+    FILE *fp;
+    fp = fopen("/sys/class/power_supply/BAT0/uevent", "r");
 
-    // get the battery percentage, always printed 4th
-    int i = 0;
-    for (; i < 3; i++) {
-        pch = strtok(NULL, " ");
-    }
-    pch = strtok(pch, "%");
+    char *line;
+    size_t nbytes = 0;
+    char result[100];
 
-    return atoi(pch);
+    // Gets the first line which stores name
+    getline(&line,&nbytes,fp);
+
+    // Gets the next line which stores the status
+    getline(&line,&nbytes,fp);
+    get_ith_split(line, "=", 1, result);
+
+    if(strcmp(result, "Charging\n") == 0)
+        batt->status = 1;
+    else batt->status = 0;
+
+    // Is power supply present
+    getline(&line,&nbytes,fp);
+
+    // Power supply technology
+    getline(&line,&nbytes,fp);
+
+    // Power supply cycle count
+    getline(&line,&nbytes,fp);
+
+    // Power supply min voltage design
+    getline(&line,&nbytes,fp);
+
+    // Power supply voltage now
+    getline(&line,&nbytes,fp);
+
+    // Power supply power now
+    getline(&line,&nbytes,fp);
+
+    // Power supply full design energy
+    getline(&line,&nbytes,fp);
+    get_ith_split(line, "=", 1, result);
+
+    batt->energy_full_design = (float) atoi(result);
+
+    // Power supply energy when full
+    getline(&line,&nbytes,fp);
+    get_ith_split(line, "=", 1, result);
+
+    batt->energy_full = (float) atoi(result);
+
+    // Power supply energy now
+    getline(&line,&nbytes,fp);
+    get_ith_split(line, "=", 1, result);
+
+    batt->energy_now = (float) atoi(result);
+
+    // Power supply capacity
+    getline(&line,&nbytes,fp);
+
+    // Power supply capacity level
+    getline(&line,&nbytes,fp);
+
+    //Power supply model name
+    getline(&line,&nbytes,fp);
+
+    // Power supply manufacturer
+    getline(&line,&nbytes,fp);
+
+    // Power supply serial number
+    getline(&line,&nbytes,fp);
+
+    free(line);
+    return 0;
 }
 
 // Used to create the notification command which is sent via system
@@ -62,26 +130,22 @@ void send_notification(int battery_level, int timeout) {
 
 int main(int argc, char * argv[]) {
 
-    char battery_string[1023];
-    int battery_percent = 0;
+    float battery_percent = 0;
+    struct Battery batt;
 
     while(1) {
-        if (get_battery_string(battery_string)) return 1;
+        get_battery_info(&batt);
+        battery_percent = (batt.energy_now/batt.energy_full_design)*100;
 
-        battery_percent = get_battery_percent(battery_string);
-        if(battery_string < 0) {
-            return 1;
-        }
+        printf("%f\n", battery_percent);
 
-        if (battery_percent < LOW)
+        if (battery_percent < CRITICAL) {
+            send_notification(CRITICAL, 5000);}
+        else if (battery_percent < LOW)
             send_notification(LOW, 5000);
-        else if (battery_percent < CRITICAL)
-            send_notification(CRITICAL, 5000);
 
         sleep(UPDATE_RATE);
     }
 
     return 0;
-
-
 }
